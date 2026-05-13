fdde45d3aa99826183e232900ca5508e62c6c0f3
Diogo-Bellini <diogocvazbellini@icloud.com>
2026-05-13 16:07:44 -0300

    adding basic db configs

--- app/src/main/java/br/dc/ufscar/devmobile/MainActivity.kt
+++ app/src/main/java/br/dc/ufscar/devmobile/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.room.Room
+import br.dc.ufscar.devmobile.configs.UPeekDatabase
 import br.dc.ufscar.devmobile.ui.theme.UPeekTheme
 
 class MainActivity : ComponentActivity() {
---
+++ app/src/main/java/br/dc/ufscar/devmobile/configs/UPeekDatabase.kt
@@ -0,0 +1,14 @@
+package br.dc.ufscar.devmobile.configs
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import br.dc.ufscar.devmobile.daos.ReserveDao
+import br.dc.ufscar.devmobile.daos.UserDao
+import br.dc.ufscar.devmobile.entities.Reserve
+import br.dc.ufscar.devmobile.entities.User
+
+@Database(entities = [User::class, Reserve::class], version = 1)
+abstract class UPeekDatabase : RoomDatabase(){
+    abstract fun userDao() : UserDao
+    abstract fun reserveDao() : ReserveDao
+}
